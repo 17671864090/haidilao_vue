bc97a239e54c65db41a3c12f666d515dec8fbff9
kan dong <53746237@qq.com>
2019-12-31 17:34:54 +0800

    创建项目

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ module.exports={
         proxy: {
             '/api': {
                 // target: 'http://127.0.0.1:3009',
-                target: 'http://60.205.178.222:3009',
+                target: 'http://60.205.178.222:8848',
                 changeOrigin: true,
                 pathRewrite: {
                     '^/api': ''
