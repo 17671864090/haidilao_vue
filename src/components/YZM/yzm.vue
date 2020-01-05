bd9817150a3dcd42f726c189be02e0c1d9b099c4
kan dong <53746237@qq.com>
2020-01-05 14:20:19 +0800

    1.新增一键删除订单/卡密功能 2.优化系统 更新时间:2020年1月5日14:20:10

--- src/components/YZM/yzm.vue
+++ src/components/YZM/yzm.vue
@@ -1,7 +1,7 @@
 <template>
     <div style="width: 100vw">
         <van-search v-model="value" placeholder="请输入授权码" show-action shape="round">
-            <div slot="action" @click="getNubber">获取手机号</div>
+            <div slot="action" @click="getNubber">获取号码</div>
         </van-search>
         <div style="height: 20px"></div>
         <el-card class="box-card">
@@ -10,14 +10,14 @@
 <!--                    <el-option v-for="item in options" :key="item.value" :label="item.label" :value="item.value"></el-option>-->
 <!--                </el-select>-->
                 <div style="height: 20px"></div>
-                <el-input style="width: 90vw" v-model="YAM_phone" placeholder="手机号(获取/指定)"></el-input>
+                <el-input style="width: 90vw" v-model="YAM_phone" placeholder="手机号"></el-input>
                 <div style="height: 20px"></div>
-                <el-button round style="width: 90vw" @click="GetYzm2Str">获取验证码</el-button>
+                <el-button round style="width: 90vw" @click="GetYzm2Str">点击获取验证码</el-button>
             </div>
         </el-card>
         <div style="height: 20px"></div>
-        <textarea style="height: 200px;width: 100vw"
-                placeholder="验证码内容"
+        <textarea style="height: 200px;width: 99vw;margin: 0 auto"
+                placeholder="接收验证码短信："
                 v-model="textarea">
         </textarea>
     </div>
@@ -26,7 +26,6 @@
 <script>
     import { GetHM2Str , binding , GetYzm2Str} from '../../api/apilist'
     import { Toast } from 'vant';
-
     export default {
         name: "yzm",
         data(){
@@ -37,58 +36,73 @@
                 }],
                 textarea: '',
                 YAM_phone:"",
-                value:null,
-                startInterval:null
+                value:599940,
+                startInterval:null,
+                startInterval1:null
             }
         },
         methods:{
-
-            onSearch(){
-                alert('验证成功')
-            },
-
-
             app(){
                 this.startInterval  = setInterval(()=>{
                     binding({data:this.YAM_phone})
                 }, 5000);
             },
-
-
-
             async getNubber(){
+                window.clearInterval(this.startInterval1)
 
+                //检测token是否正确
                 GetHM2Str({token:this.value}).then((res=>{
-
                     if(res.code == 200){
-                    this.YAM_phone = res.YAM_phone
-
-                    clearInterval(this.startInterval)
-
-
-                    this.app()
-
+                        this.YAM_phone = res.YAM_phone
+                        if(this.startInterval){
+                            clearInterval(this.startInterval)
+                            this.app()
+                        }else{
+                            this.app()
+                        }
                     }else{
                         alert(res.msg)
                     }
                 }))
-
-
-
             },
             async GetYzm2Str(){
-                Toast('验证码获取中,请稍后');
-                const id = setInterval(()=>{
+                //检测token是否正确
+                GetHM2Str({token:this.value}).then((res=>{
+                    if(res.code == 200){
+                        Toast('验证码获取中,请稍后');
+                        if(this.startInterval1){
+                            clearInterval(this.startInterval1)
+                            this.app1()
+                        }else{
+                            this.app1()
+                        }
+                    }else{
+                        alert(res.msg)
+                    }
+                }))
+            },
+            async app1(){
+                this.startInterval1 = setInterval(()=>{
                     this.Vue_GetYzm2Str().then((res=>{
-                        console.log(res)
-                       if(res.code == 200){
-                           Toast('获取成功');
-                           window.clearInterval(id)
-                           this.textarea = res.data
-                       }
+                        console.log(res.data)
+                        if(res.code == 200){
+                            Toast('获取成功');
+                            clearInterval(this.startInterval1)
+                            this.textarea = res.data
+                        }else{
+
+                            var myDate = new Date();
+
+                            var mytime=myDate.toLocaleTimeString();
+
+
+                            this.textarea = "最新状态码:" + res.msg + "当前时间" + mytime;
+                        }
                     }))
                 }, 5000);
             },
+
+
             async Vue_GetYzm2Str(){
 
                 const data = {
